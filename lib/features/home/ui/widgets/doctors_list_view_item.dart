5a4ef9bec8d148062f9755509c8cabf53fa8fec0
Zyad Ahmed <Zyad.ahmed.Za55@gmail.com>
2025-12-11 04:11:09 +0200

    -test

--- lib/features/home/ui/widgets/doctors_list_view_item.dart
+++ lib/features/home/ui/widgets/doctors_list_view_item.dart
@@ -1,8 +1,11 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:doctor_app/core/helpers/spacing.dart';
+import 'package:doctor_app/core/theming/colors.dart';
 import 'package:doctor_app/core/theming/styles.dart';
 import 'package:doctor_app/features/home/data/models/specializations_response_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shimmer/shimmer.dart';
 
 class DoctorsListViewItem extends StatelessWidget {
   final Doctor? doctorModel;
@@ -15,15 +18,7 @@ class DoctorsListViewItem extends StatelessWidget {
       margin: EdgeInsets.only(bottom: 16.h),
       child: Row(
         children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(12),
-            child: Image.network(
-              "https://static.wikia.nocookie.net/five-world-war/images/6/64/Hisoka.jpg/revision/latest?cb=20190313114050",
-              width: 110.w,
-              height: 120.h,
-              fit: BoxFit.cover,
-            ),
-          ),
+          buildImage(),
           horizontalSpace(16),
           Expanded(
             child: Column(
@@ -48,6 +43,37 @@ class DoctorsListViewItem extends StatelessWidget {
             ),
           ),
         ],
+      ),
+    );
+  }
+
+  CachedNetworkImage buildImage() {
+    return CachedNetworkImage(
+      imageUrl:
+          "https://static.wikia.nocookie.net/five-world-war/images/6/64/Hisoka.jpg/revision/latest?cb=20190313114050",
+      progressIndicatorBuilder: (context, url, downloadProgress) {
+        return Shimmer.fromColors(
+          baseColor: ColorsManager.lightGray,
+          highlightColor: Colors.white,
+          child: Container(
+            width: 110.w,
+            height: 120.h,
+            decoration: BoxDecoration(
+              shape: BoxShape.rectangle,
+              borderRadius: BorderRadius.circular(12.0),
+              color: Colors.white,
+            ),
+          ),
+        );
+      },
+      imageBuilder: (context, imageProvider) => Container(
+        width: 110.w,
+        height: 120.h,
+        decoration: BoxDecoration(
+          shape: BoxShape.rectangle,
+          borderRadius: BorderRadius.circular(12.0),
+          image: DecorationImage(image: imageProvider, fit: BoxFit.cover),
+        ),
       ),
     );
   }
